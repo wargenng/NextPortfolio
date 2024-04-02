b8526cf503ef5a9293db7d35502aade700a38e92
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-04-01 21:12:00 -0700

    adding icons

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,3 +1,17 @@
+import { CgAddR } from "react-icons/cg";
+
 export default function Home() {
-    return <div>hello world</div>;
+    function changeSymbol(event) {
+        console.log(event);
+    }
+    return (
+        <div className="flex items-center justify-center w-screen h-screen">
+            <div className="flex items-center gap-2">
+                <button onClick={changeSymbol()}>
+                    <CgAddR />{" "}
+                </button>
+                wargen guittap
+            </div>
+        </div>
+    );
 }
