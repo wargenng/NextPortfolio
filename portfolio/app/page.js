8f56a37a834d1534f97f8b9b7dead4b669b9b751
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-04-01 23:30:37 -0700

    finished week 1

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="flex items-center justify-center w-screen h-screen">
+        <div className="flex justify-center w-screen h-screen">
             <Link href="/classes">Classes</Link>
         </div>
     );
