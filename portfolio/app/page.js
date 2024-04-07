9fbe2a29e46c5ad7ccc72561c52347f4b54608f1
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-04-06 22:11:20 -0700

    added home screen

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,9 +1,40 @@
+"use client";
 import Link from "next/link";
+import About from "./components/about.mdx";
+
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuList,
+} from "@/components/ui/navigation-menu";
 
 export default function Home() {
     return (
-        <div className="flex justify-center w-screen h-screen">
-            <Link href="/classes">Classes</Link>
+        <div className="flex justify-center w-screen flex-col">
+            <div className="w-full p-6 text-lg flex text-slate-500">
+                <Link href="/classes" className="grow">
+                    woreg.in
+                </Link>
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        <Link href="/classes" className="px-2">
+                            <NavigationMenuItem>Courses</NavigationMenuItem>
+                        </Link>
+                        <a href="https://github.com/wargenng">
+                            <NavigationMenuItem>GitHub</NavigationMenuItem>
+                        </a>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </div>
+            <h1 className="text-center text-4xl font-semibold ">
+                Wargen Guittap
+            </h1>
+            <h2 className="text-center text-lg text-slate-500 py-3">
+                Software Engineer, Data Analyst
+            </h2>
+            <div className="prose flex flex-col p-6">
+                <About />
+            </div>
         </div>
     );
 }
