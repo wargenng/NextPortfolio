7e0641bc175f4942944c7f79c172512b052a1e02
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-04-06 18:15:37 -0700

    added lesson plans to the other introduction class

---
+++ portfolio/app/classes/javascript/introduction/04075A/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+export default function Page() {
+    const path = usePathname();
+    return (
+        <div className="flex flex-col items-center w-screen h-screen p-8">
+            <Link href={`${path}/week1`}>
+                Week 1: Introduction to JavaScript
+            </Link>
+        </div>
+    );
+}
---
+++ portfolio/app/classes/javascript/introduction/04075A/week4/page.js
@@ -0,0 +1,9 @@
+import Week from "./week4.mdx";
+
+export default function Page() {
+    return (
+        <div className="prose flex flex-col p-8">
+            <Week />
+        </div>
+    );
+}
