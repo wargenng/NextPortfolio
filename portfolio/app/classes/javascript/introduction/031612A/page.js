bb161642ff3469ce1b666585b0727c283216d1a1
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-04-06 18:14:12 -0700

    fixed path

--- portfolio/app/classes/javascript/introduction/031612A/page.js
+++ portfolio/app/classes/javascript/introduction/031612A/page.js
@@ -1,14 +1,22 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function Page() {
+    const path = usePathname();
     return (
         <div className="flex flex-col items-center w-screen h-screen p-8">
-            <Link href="/week1">Week 1: Introduction to JavaScript</Link>
-            <Link href="/week2">
+            <Link href={`${path}/week1`}>
+                Week 1: Introduction to JavaScript
+            </Link>
+            <Link href={`${path}/week2`}>
                 Week 2: Control Flow, Functions, and Arrow Functions
             </Link>
-            <Link href="/week3">Week 3: Data Structures in JavaScript</Link>
-            <Link href="/week4">
+            <Link href={`${path}/week3`}>
+                Week 3: Data Structures in JavaScript
+            </Link>
+            <Link href={`${path}/week4`}>
                 Week 4: Document Object Model (DOM) Manipulation
             </Link>
         </div>
--- portfolio/app/classes/page.js
+++ portfolio/app/classes/page.js
@@ -1,3 +1,5 @@
+"use server";
+
 import Link from "next/link";
 import { promises as fs } from "fs";
 
