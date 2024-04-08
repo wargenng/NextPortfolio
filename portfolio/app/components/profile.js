5432aab2732e7abb04d3593546f52f9e08704069
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-04-08 13:37:38 -0700

    added week 2 lecture notes

---
+++ portfolio/app/components/profile.js
@@ -0,0 +1,11 @@
+export default function Profile() {
+    return (
+        <Image
+            src="/profile.png"
+            width={200}
+            height={200}
+            alt="Picture of the author"
+            className="py-3"
+        />
+    );
+}
