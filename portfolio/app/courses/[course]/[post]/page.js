6a6c9db018eb5a0a64c21d4972769eac4935edbc
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-04-13 23:59:32 -0700

    adding in pagination

--- portfolio/app/courses/[course]/[post]/page.js
+++ portfolio/app/courses/[course]/[post]/page.js
@@ -1,10 +1,19 @@
 import dynamic from "next/dynamic";
 import { courses } from "../../courses";
+import Pagi from "../../components/pagination";
 
 export default async function Page({ params }) {
     const course = courses.classes.find(
         (course) => course.classId === params.course
     );
+
+    function findIndexOfLesson(data, lessonName) {
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].lesson.includes(lessonName)) {
+                return i; // Return the index of the first match
+            }
+        }
+    }
 
     const Post = dynamic(
         () => import(`./${course.subject}/${params.post}.mdx`),
@@ -12,9 +21,14 @@ export default async function Page({ params }) {
             ssr: false,
         }
     );
+    const currentLesson =
+        course.lessons[findIndexOfLesson(course.lessons, params.post)];
+    const currentPage = currentLesson.lesson.indexOf(params.post);
+
     return (
         <div className="prose flex flex-col p-8 w-full h-full">
             <Post />
+            <Pagi lesson={currentLesson.lesson} page={currentPage} />
         </div>
     );
 }
---
+++ portfolio/app/courses/components/pagination.js
@@ -0,0 +1,53 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { VscChevronLeft, VscChevronRight } from "react-icons/vsc";
+
+export default function Pagi(props) {
+    const path = usePathname().split("/").slice(0, -1).join("/") + "/";
+    console.log(path);
+    console.log(props.lesson);
+    console.log(props.page);
+
+    const previousPath =
+        props.page === 0 ? path : path + props.lesson[props.page - 1];
+    const previousName =
+        props.page === 0
+            ? "Back to " + path.split("/").at(-2)
+            : capitalizeFirstLetter(props.lesson[props.page - 1]);
+    const nextPath =
+        props.page === props.lesson.length - 1
+            ? path
+            : path + props.lesson[props.page + 1];
+    const nextName =
+        props.page === props.lesson.length - 1
+            ? "Back to " + path.split("/").at(-2)
+            : capitalizeFirstLetter(props.lesson[props.page + 1]);
+
+    return (
+        <div className="w-full flex py-5">
+            <Link href={previousPath}>
+                <div className="flex justify-center items-center">
+                    <VscChevronLeft />
+                    <p className="pl-2">{previousName}</p>
+                </div>
+            </Link>
+
+            <div className="grow" />
+            <Link href={nextPath}>
+                <div className="flex justify-center items-center">
+                    <p className="pr-2">{nextName}</p>
+                    <VscChevronRight />
+                </div>
+            </Link>
+        </div>
+    );
+}
+
+export function capitalizeFirstLetter(str) {
+    return str
+        .split("-")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+}
