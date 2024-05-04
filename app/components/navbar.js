b1118597597be0176abf72bce6b556478a36415a
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-05-04 11:38:48 -0700

    changing name

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -10,7 +10,7 @@ import {
 export default function NavBar() {
     return (
         <div className="w-full p-4 text-lg flex text-primary">
-            <Link href="/">woreg.in</Link>
+            <Link href="/">wargen guittap</Link>
             <div className="grow"></div>
             <NavigationMenu>
                 <NavigationMenuList>
